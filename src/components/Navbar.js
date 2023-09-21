557a4bbcdaf42ec00b2765c6560bc26be3184d8f
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-09-21 11:50:15 +0300

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
     <div className={`navbar ${darkMode ? 'dark' : ''}`}>
       <div className="flex max-w-[1240px] mx-auto items-center p-4 justify-between">
         <div className="cursor-pointer">
-          <h1 className="font-bold text-3xl sm:text-4xl lg:text-4xl">Eatify</h1>
+          <h1 className="font-bold text-3xl sm:text-4xl lg:text-4xl">Abvss Fast Food</h1>
         </div>
         <div className="flex">
           <div className={`flex justify-center h-10 rounded-md sm:pl-2 sm:w-[400px] lg:w-[400px] items-center ${darkMode ? 'dark' : ''}`}>
